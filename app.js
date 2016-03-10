0aca752a1cfa231cd9495bc91568613c2c230910
Gorschel <bananenbaumi@web.de>
2016-03-10 19:47:37 +0100

    df

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-$('.auswahl').css('display', 'block'); 
-$('.scanner').css('display', 'none');
 
-$(document).ready(function(){       //kamera
-    $('.camera').html5_qrcode(function(data) {
-        
-  if(blocks[currentBlockIndex].name == data) { //prüfen ob das passende gescannt wurde
+
+var start_scanner = function(){       //scanner starten
+  $('.camera').html5_qrcode(function(data) {
+}, function() {
+}, function() {
+
+if(blocks[currentBlockIndex].name == data) { //prüfen ob das passende gescannt wurde
     // grün
   } else {
     // rot
   }
 
-}, function() {
-}, function() {
-
 });
-});                  //kamera ende
+};                  //kamera ende
 
 var blocks;
 var currentBlockIndex;
@@ -23,28 +21,20 @@ var tetrisBlocks = {
   "struktur2": [{name: "4gelb", count: 1},{name: "4gruen", count: 1},{name: "4blau", count: 1}, {name: "8rot", count: 2}]
 };
 
-
+$(document).ready(function(){
 $('.auswahl img').on('click', function() {  //wenn auf eins der bilder geklickt wird
   $('.auswahl').css('display', 'none'); //auswahlbildschirm wird ausgeblendet
   $('.scanner').css('display', 'block'); // scanner wird eingeblendet (breitseitig)
+  // start_scanner();   wenn scanner aktiv sein soll
   var blockName = $(this).data('block'); //welcher wurde angeklickt
   blocks = tetrisBlocks[blockName];  //array wird in blocks geschrieben
   currentBlockIndex = 0;  //index=0
 });
+});
+
 
 
 $('.scanner .confirm-button').on('click', function() {
    currentBlockIndex++; // schrittnummer wird um 1 erhöht
 });
 
-//scan 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
