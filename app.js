fb4de11bb836e9465ffe7bc780488f7ae6df4907
Gorschel <bananenbaumi@web.de>
2016-03-17 18:36:02 +0100

    cowo3 17.3.

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-var start_scanner = function(){                             //scanner starten
+var start_scanner = function(){                             
   $('.camera').html5_qrcode(function(data) {
-    $('.scr').text("letzte: ",data)                               //text ausgeben
+    $('.scr').text("letzte: "+data);                        
     
     if(blocks[currentBlockIndex].name == data) {            //prüfen ob das passende gescannt wurde
-        $('.scr').css ("color", "green")                    //bg=grün // sucht nach html element, bei result->wird css element auf () gesetzt
+        $('.result').css ("background", "green")
+        $('#confirm-button').css('display', 'block')                    
     } else {
-        $('.scr').css ("color", "red")                      // bg=rot
+        $('.result').css ("background", "red")                      
     }
-        
     }, function() {
     }, function() {
   });
-};                                                          //"kamera"" ende
+};                                                          
 
 var blocks;                                                 // ?
 var currentBlockIndex;                                      // schrittnummer
-//var s2_1 = $('s2.1');
-//var s2_2 = $('s2.2');
-//var s2_3 = $('s2.3');
-var blockstructure = {                                      // Aufbaureihenfolge der strukturen
+var blockstructure = {                                      
   "struktur1": [{name: "4gelb", count: 1},
                 {name: "4rot", count: 1}],
   "struktur2": [{name: "4gelb", count: 1},
@@ -26,22 +23,19 @@ var blockstructure = {                                      // Aufbaureihenfolge
                 {name: "4blau", count: 1}, 
                 {name: "8rot", count: 2}]
 };
-$('.confirm-button').on('click', function() {               // auf confirm klicken
-   currentBlockIndex++;                                     // schrittnummer wird um 1 erhöht
-});
 
-$(document).ready(function(){                               // wenn html geladen, dann Funktion ausführen
- $('.bauschritte').css('display', 'none')                   // bauschritte struct2 ausblenden
- $('.auswahl img').on('click', function() {                 // wenn auf eins der bilder geklickt wird
-    $('.auswahl').css('display', 'none');                   // auswahlbildschirm wird ausgeblendet
-    $('.scanner').css('display', 'block');                  // scanner wird eingeblendet (breitseitig)
-  start_scanner();                                          // wenn scanner aktiv sein soll
+$(document).ready(function(){                               
+  $('.auswahl img').on('click', function() {                 // wenn auf eins der bilder geklickt wird
+    $('.auswahl').css('display', 'none');                   
+    $('.scanner').css('display', 'block');                  
+  start_scanner();                                          
   var blockName = $(this).data('block');                    // welcher wurde angeklickt
     blocks = blockstructure[blockName];                     // array wird in blocks geschrieben
-    currentBlockIndex = 0;                                  // index=0 setzen
-     
-  
-    
-    
+    currentBlockIndex = 0;                                  
+  $('#confirm-button').on('click', function(){
+    $('.scanner').css('display', 'none')
+    if $('#confirm-button').css('display', 'none')
+    currentBlockIndex++                                  
+  });    
 });
 });
